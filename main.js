82a073b30e4903b3c9bf8a1eaa6286526f38ccf5
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2017-12-04 11:01:17 +0000

    bugfix

--- main.js
+++ main.js
@@ -1,11 +1,59 @@
 
-/*---------VIDEO--------- */
+// FUNCTION FOR CLICKING VIDEO:
+function moveToSecondScreen() {
+  // fades out video over 1.5s
+  $('.player').fadeOut(1500, function() {
+    $(this).find('video').remove();
+  });
+  // fades out sound over 3.5 seconds (lingering effect)
+  $('.sound').animate({volume: 0.0}, 3500);
+  // hides custom cursor image
+  $('.cursor-custom').hide();
+}
+
+/*---------------------------------------------
+CUSTOM CURSOR:
+----------------------------------------------*/
+// CUSTOM CURSOR (IMAGE FOLLOWING MOUSE):
+$('.video').mousemove(function (e) {
+  $('.cursor-custom').show().css({
+    "left": e.clientX,
+    "top": e.clientY
+  });
+})
+
+/*---------------------------------------------
+VIDEO:
+----------------------------------------------*/
 // CLICKING VIDEO:
 $('.video').on('click touchstart', function() {
   moveToSecondScreen();
 });
 
+// END OF VIDEO:
+$('.video').on('ended',function(){
+  // fades out video over 1.5 seconds
+  $('.player').fadeOut(1500);
+  // fades out sound over 3.5 seconds (lingering effect)
+  $('.sound').animate({volume: 0.0}, 3500);
+});
 
+/*---------------------------------------------
+SOUND:
+---------------------------------------------- */
+// ON LOAD:
+$(document).ready( function () {
+  if ($(window).width() > 768) {
+    var s = $('.sound')
+    s.attr('preload', 'true');
+    s.attr('autoplay', 'true');
+  }
+})
+
+/*---------------------------------------------
+ACCORDION:
+----------------------------------------------*/
+// WHEN CLICKING A HEADING TITLE
 $('.accordion .title').on('click', function() {
 
   if ($(this).hasClass('expand')) {
@@ -24,44 +72,4 @@ $('.accordion .title').on('click', function() {
   otheraccordions.find('.intro').slideUp();
   otheraccordions.removeClass('open');
 
-})
-
-
-/*---------------------------------------------
-INTERACTIONS DEPENDING ON THE WIDTH OF THE WINDOW:
----------------------------------------------- */
-// READING THE WIDTH OF THE WINDOW ON LOAD:
-$(document).ready( function () {
-  if ($(window).width() > 768) {
-    var s = $('.sound')
-    s.attr('preload', 'true');
-    s.attr('autoplay', 'true');
-  }
-})
-
-/*---------CUSTOM CURSOR--------- */
-// CUSTOM CURSOR (IMAGE FOLLOWING MOUSE):
-$('.video').mousemove(function (e) {
-  $('.cursor-custom').show().css({
-    "left": e.clientX,
-    "top": e.clientY
-  });
-})
-
-
-function moveToSecondScreen() {
-  // fades out video over 1.5s
-  $('.player').fadeOut(1500, function() {
-    $(this).find('video').remove();
-  });
-  // fades out sound over 3.5 seconds (lingering effect)
-  $('.sound').animate({volume: 0.0}, 3500);
-  // hides custom cursor image
-  $('.cursor-custom').hide();
-}
-
-
-// /*---------ON LOAD--------- */
-$(document).ready( function () {
-  // moveToSecondScreen();
 })
